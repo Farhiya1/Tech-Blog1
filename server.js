47708f4a08495a67c971d05ae51aec0123ae37a1
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-04-02 19:04:47 +0100

    Connecting databse to server

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ const exphbs = require("express-handlebars");
 // Creating an express application
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Connecting database to server
+const sequelize = require("./config/connection.js");
+
