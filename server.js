062ae62fd76413b6740ffce8f0595c7d980f22f4
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-04-02 19:05:29 +0100

    Authenticating database and connecting to sessions

--- server.js
+++ server.js
@@ -12,3 +12,20 @@ const PORT = process.env.PORT || 3001;
 // Connecting database to server
 const sequelize = require("./config/connection.js");
 
+// Authenticating database and connects to sessions
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  // Secret code between server and user
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+// As soon as server is contacted, session is started.  Session generates a cookie.
+app.use(session(sess));
+
