2406541408a10fae0b69a1fa5ad1d3a3d5674c9d
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-04-02 22:55:45 +0100

    home-routes created

---
+++ controllers/home-routes.js
@@ -0,0 +1,65 @@
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models");
+
+// Get all posts for the homepage
+router.get("/", (req, res) => {
+  Post.findAll({
+    include: [User],
+  }) // Serialize data so the template can read it
+    .then((postDBData) => {
+      const posts = postDBData.map((post) => post.get({ plain: true }));
+
+      res.render("all-posts", { posts });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+// Get single post
+router.get("/post/:id", (req, res) => {
+  Post.findByPk(req.params.id, {
+    include: [
+      User,
+      {
+        model: Comment,
+        include: [User],
+      },
+    ],
+  })
+    .then((postDBData) => {
+      if (postDBData) {
+        const post = postDBData.get({ plain: true });
+
+        res.render("single-post", { post });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+// Login
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
+module.exports = router;
